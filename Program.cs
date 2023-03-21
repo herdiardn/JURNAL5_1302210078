98f1d9d9d48c22ec6dd008cad5903680a857a742
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-03-21 16:35:21 +0700

    “menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Penjumlahan
+{
+    public static T jumlahTigaAngka<T>(T One, T Two, T Three) {
+        return ((dynamic)One + (dynamic)Two + (dynamic)Three);
+    }
+}
+
+public class Program { 
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Penjumlahan.jumlahTigaAngka<int>(13, 2, 21));
+
+    }
+}
